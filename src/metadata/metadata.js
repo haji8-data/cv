dd919da041e6bfdbafb2934bf5d1caa17e056d80
HAJI <gwd7454@gmail.com>
2021-08-30 07:03:19 +0100

    update resume

--- src/metadata/metadata.js
+++ src/metadata/metadata.js
@@ -10,22 +10,29 @@ module.exports = {
   },
   skills: [
     ['python', '++'],
-    ['java', '++'],
-    ['Hadoop', '++'],
-    ['Kafka', '++'],
-    ['ElasticSearch', '++'],
-    ['AWS Data', '+++'],
     ['Javascript', '++'],
     ['Typescript', '+'],
+    ['Vue.js', '+'],
+    ['ElasticSearch', '+++'],
+    ['java', '++'],
+    ['Athena', '+++'],
+    ['QuickSight', '+++'],
+    ['Firehose', '+++'],
+    ['SageMaker', '+++'],
+    ['Hadoop', '++'],
+    ['Kafka', '++'],
+    ['Flume', '++'],
+    ['Mysql', '++'],
+    ['MongoDB', '++'],
   ],
   positions: [
     {
       company: 'BALAAN',
       location: 'Nonhyeon',
-      title: 'Data fullstack scientist',
+      title: 'Data Developer',
       period: 'may 2020 - present',
       skills: ['JavaScript', 'NodeJs', 'mongodb', 'mysql', 'typescript','python','ElasticSearch','AWS QuickSight','AWS Data services'],
-      contents: ``,
+      contents: `발란 전사 데이터를 관리하는 데이터팀`,
     },
     {
       company: 'NAVER Corp',
@@ -41,7 +48,7 @@ module.exports = {
       title: 'Java Software Engineer',
       period: 'Mar 2014 - Apr 2016',
       skills: ['java','mybatis','spring','flex','sencha touch', 'codova', 'oracle','mysql','junit','Jenkins','Sonar Qube', 'MediaWiki'],
-      contents: ``,
+      contents: `관제 시스템의 코어부를 담당하는 신기술 파트`,
     },
     {
       company: 'Soon Electronic',
@@ -49,35 +56,96 @@ module.exports = {
       title: 'Android Developer',
       period: 'Mar 2013 - Feb 2014',
       skills: ['안드로이드 어플 개발', '멀티미디어 압축/편집'],
-      contents: ``
+      contents: `안드로이드 어플 개발 담당`
     },
   ],
   education:[
     {
-       period:'2010 - 2014',
-       university:'Kumoh National Institute of Technology',
-       major:'Computer Software Engineering'
+      period:'2010 - 2014',
+      university:'Kumoh National Institute of Technology',
+      major:'Computer Software Engineering'
+    }
+  ],
+  Release:[
+    {
+      type:'thesis',
+      name:'PC Virtual I/O Device Development using Android Smartphone',
+      url:'https://www.eiric.or.kr/literature/ser_view.php?SnxGubun=INKO&mode=total&searchCate=literature&gu=INME001E9&cmd=qryview&SnxIndxNum=152361&rownum=&totalCnt=1&rownum=1&q1_t=7JWI65Oc66Gc7J2065OcIOyKpOuniO2KuO2PsOydhCDsnbTsmqntlZwgUEMg6rCA7IOBIOyehey2nOugpSDs',
+      date:'Jan 2013',
+    },
+    {
+      type:'patent',
+      name:'Resource information management and data storage system through the management of the resource template',
+      url:'http://kpat.kipris.or.kr/kpat/biblioa.do?method=biblioFrame&start=biblio&searchFg=N',
+      date:'Dec 2015',
     }
   ],
   certifications:[
+
     {
-       icon:'',
-       title:'SDI DIVE MASTER',
-       date:'Mar 2020'
+      icon:'',
+      url:'http://www.q-net.or.kr/crf005.do?id=crf00503&jmCd=1320&gSite=Q&gId',
+      title:'정보처리기사',
+      date:'Nov 2013'
+    },
+    {
+      icon:'',
+      url:'',
+      title:'OPIC IL',
+      date:'Jan 2014'
     },
     {
        icon:'',
-       url:'http://tes.co',
+       url:'',
        title:'JPT 575/990',
        date:'Oct 2018'
     },
     {
-       icon:'',
-       url:'http://tes.com',
-       title:'정보처리기사',
-       date:'Nov 2013'
+      icon:'',
+      url:'https://www.tdisdi.com/sdi/get-certified/divemaster-course/',
+      title:'SDI DIVE MASTER',
+      date:'Mar 2020'
+    },
+  ],
+  AdditionalExperience:[
+    {
+      type: 'activity',
+      name: '금오공과대학교 YMCA 봉사 동아리',
+      url: '',
+      date: 'Mar 2010 ~ Feb 2013',
+    },
+    {
+      type: 'activity',
+      name: '제 2 대 컴퓨터 소프트웨어 공학과 학생회, 회장',
+      url: '',
+      date: 'Mar 2013 ~ Feb 2014',
+    },
+    {
+      type: 'theater',
+      name: '극단 빈공간, 한 여름밤의 꿈, 스누그역',
+      url: 'https://www.youtube.com/watch?v=gLPWCMX1eKs',
+      date: 'Aug 2015',
+    },
+    {
+      type: 'activity',
+      name: '코이카 해외 봉사단체, 국내 교육',
+      url: '',
+      date: 'Mar 2019 ~ Nov 2018',
+    },
+  ],
+  Award:[
+    {
+      name: '캡스톤 디자인 경진 대회 장려-RMD for Android',
+      url:'http://gmilbo.net/news/article.html?no=21412',
+      date:'Nov 2011',
+    },
+    {
+      name: '캡스톤 디자인 경진 대회 금상-Bubble Finder',
+      url:'',
+      date:'Nov 2012',
     },
   ]
+
 
 };
 
