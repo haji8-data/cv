2f12c38033a7e627aada3a7fa260b3aa1effe070
haji8-de <gwd7454@gmail.com>
2022-04-21 23:08:06 +0900

    update looko

---
+++ README.md
@@ -0,0 +1,9 @@
+
+## Installation
+
+1. Install project dependencies with `npm install`.
+1. Run `npm run deploy` to initialize `gh-pages`. This is a one time action. Further deployments will be initiated by GitHub Actions on every push to `master`.
+
+## Usage
+
+1. Start local development server with `npm start`.s
--- src/metadata/metadata.js
+++ src/metadata/metadata.js
@@ -2,20 +2,20 @@ module.exports = {
   name: 'Jiyang Ha',
   title: 'Data Engineer',
   lang:'kor',
-  baseUrl: 'https://sneas.github.io/cv/',
   facts: {
     Residence: 'Seoul, South Korea',
     LinkedIn: '<a href="http://www.linkedin.com/in/haji8"><i class="fab fa-linkedin fact-icon"></i> haji8 </a>',
-    GitHub: '<a href="https://github.com/haji8-data"><i class="fab fa-github fact-icon"></i> haji8-data </a>',
+    GitHub: '<a href="https://github.com/haji8-de"><i class="fab fa-github fact-icon"></i> haji8-de </a>',
     Email: '<a href="mailto:gwd7454@gmail.com">gwd7454@gmail.com</a>',
   },
   skills: [
-    ['python', '++'],
+    ['python', '+++'],
     ['Javascript', '++'],
-    ['Typescript', '+'],
-    ['Vue.js', '+'],
+    ['Vue.js', '++'],
+    ['flask', '++'],
+    ['postgrsql', '++'],
+    ['graphql', '+'],
     ['ElasticSearch', '+++'],
-    ['java', '++'],
     ['Athena', '+++'],
     ['QuickSight', '+++'],
     ['Firehose', '+++'],
@@ -28,6 +28,19 @@ module.exports = {
   ],
   positions: [
     {
+      company: 'Looko',
+      location: 'Gangnam',
+      title: 'Data Enginner',
+      period: 'February 2021 - Present',
+      skills: ['python', 'flask', 'Vue.js', 'mongodb', 'mariadb', 'postgrsql', 'Javascript'],
+      contents: `패션 AI 모델을 연구하고 고도화 하는 AI팀`,
+      detail: [
+        'AI 학습을 위한 데이터 라벨링, 검증 백오피스 개발',
+        'image type 판별/ background 제거 모델 고도화',
+        'Fashion item attribute classification model training image increasing for improve model performance'
+      ],
+    },
+    {
       company: 'BALAAN',
       location: 'Nonhyeon',
       title: 'Data Developer',
@@ -35,7 +48,10 @@ module.exports = {
       skills: ['JavaScript', 'NodeJs', 'mongodb', 'mysql', 'typescript','python','ElasticSearch','AWS QuickSight','AWS Data services'],
       contents: `발란 전사 데이터를 관리하는 데이터팀`,
       detail: [
-      'OKR 대시보드 구축을 통한 매출 관리 포인트 강화','검색 랭킹 기능 등 검색 기능 개선을 통한 사용자 편의 제공','사용자 검색 데이터 등을 신규 AWS 파이프라인으로 구축'],
+        'OKR 대시보드 구축을 통한 매출 관리 포인트 강화',
+        '검색 랭킹 기능 등 검색 기능 개선을 통한 사용자 편의 제공',
+        '사용자 검색 데이터 등을 신규 AWS 파이프라인으로 구축'
+      ],
     },
     {
       company: 'NAVER Corp',
@@ -44,7 +60,13 @@ module.exports = {
       period: 'Jul 2016 - Aug 2018',
       skills: ['Python','django','hadoop','kafka','flume','logstash','hive','ElasticSearch','spark','java'],
       contents: `네이버 전사 동영상 로그를 수집하는 동영상 플랫폼팀 데이터 파트`,
-      detail: ['ElasticSearch를 이용한 실시간 통계 파이프라인 구축','동영상 재생 데이터 등을 통계 대시보드로 구현','데이터 지표 확장을 통한 분석 포인트 증가','수집 데이터 전수 검사를 통한 데이터 정합성 향상', '재생 패턴에 따라 어뷰징 필터링 시스템 강화'],
+      detail: [
+        'ElasticSearch를 이용한 실시간 통계 파이프라인 구축',
+        '동영상 재생 데이터 등을 통계 대시보드로 구현',
+        '데이터 지표 확장을 통한 분석 포인트 증가',
+        '수집 데이터 전수 검사를 통한 데이터 정합성 향상',
+        '재생 패턴에 따라 어뷰징 필터링 시스템 강화'
+      ],
     },
     {
       company: 'WatchTek',
@@ -53,7 +75,12 @@ module.exports = {
       period: 'Mar 2014 - Apr 2016',
       skills: ['java','mybatis','spring','flex','sencha touch', 'codova', 'oracle','mysql','junit','Jenkins','Sonar Qube', 'MediaWiki'],
       contents: `관제 시스템의 코어부를 담당하는 신기술 파트`,
-      detail: ['watchall 10의 공통 프로세스 이슈 처리','물리 네트워크를 네트워크 정보를 통해 연결 정보를 알아내는 선번장 개발(특허)','소스 품질 관리 시스템, WIKI 구축', '개발팀 신기술 파트 리더'],
+      detail: [
+        'watchall 10의 공통 프로세스 이슈 처리',
+        '물리 네트워크를 네트워크 정보를 통해 연결 정보를 알아내는 선번장 개발(특허)',
+        '소스 품질 관리 시스템, WIKI 구축',
+        '개발팀 신기술 파트 리더'
+      ],
     },
   ],
   education:[
@@ -65,31 +92,30 @@ module.exports = {
   ],
   release:[
     {
-      type:'thesis',
-      title:'PC Virtual I/O Device Development using Android Smartphone',
-      url:'https://www.eiric.or.kr/literature/ser_view.php?SnxGubun=INKO&mode=total&searchCate=literature&gu=INME001E9&cmd=qryview&SnxIndxNum=152361&rownum=&totalCnt=1&rownum=1&q1_t=7JWI65Oc66Gc7J2065OcIOyKpOuniO2KuO2PsOydhCDsnbTsmqntlZwgUEMg6rCA7IOBIOyehey2nOugpSDs',
-      date:'Jan 2013',
-    },
-    {
       type:'patent',
       title:'Resource information management and data storage system through the management of the resource template',
       url:'http://kpat.kipris.or.kr/kpat/biblioa.do?method=biblioFrame&start=biblio&searchFg=N',
       date:'Dec 2015',
+    },
+    {
+      type:'thesis',
+      title:'PC Virtual I/O Device Development using Android Smartphone',
+      url:'https://www.eiric.or.kr/literature/ser_view.php?SnxGubun=INKO&mode=total&searchCate=literature&gu=INME001E9&cmd=qryview&SnxIndxNum=152361&rownum=&totalCnt=1&rownum=1&q1_t=7JWI65Oc66Gc7J2065OcIOyKpOuniO2KuO2PsOydhCDsnbTsmqntlZwgUEMg6rCA7IOBIOyehey2nOugpSDs',
+      date:'Jan 2013',
     }
   ],
   certifications:[
-
     {
       icon:'',
-      url:'http://www.q-net.or.kr/crf005.do?id=crf00503&jmCd=1320&gSite=Q&gId',
-      title:'정보처리기사',
-      date:'Nov 2013'
+      url:'hhttps://coursera.org/share/a3eec63e1a35bcc206a98a58567e0b2d',
+      title:'Coursera ML course',
+      date:'Oct 2021'
     },
     {
       icon:'',
-      url:'',
-      title:'OPIC IL',
-      date:'Jan 2014'
+      url:'https://www.tdisdi.com/sdi/get-certified/divemaster-course/',
+      title:'SDI DIVE MASTER',
+      date:'Mar 2020'
     },
     {
        icon:'',
@@ -99,24 +125,18 @@ module.exports = {
     },
     {
       icon:'',
-      url:'https://www.tdisdi.com/sdi/get-certified/divemaster-course/',
-      title:'SDI DIVE MASTER',
-      date:'Mar 2020'
+      url:'',
+      title:'OPIC IL',
+      date:'Jan 2014'
+    },
+    {
+      icon:'',
+      url:'http://www.q-net.or.kr/crf005.do?id=crf00503&jmCd=1320&gSite=Q&gId',
+      title:'정보처리기사',
+      date:'Nov 2013'
     },
   ],
   addExperience:[
-    {
-      type: 'activity',
-      title: '금오공과대학교 YMCA 봉사 동아리',
-      url: '',
-      date: 'Mar 2010 ~ Feb 2013',
-    },
-    {
-      type: 'activity',
-      title: '제 2 대 컴퓨터 소프트웨어 공학과 학생회, 회장',
-      url: '',
-      date: 'Mar 2013 ~ Feb 2014',
-    },
     {
       type: 'theater',
       title: '극단 빈공간, 한 여름밤의 꿈, 스누그역',
@@ -125,43 +145,21 @@ module.exports = {
     },
     {
       type: 'activity',
-      title: '코이카 해외 봉사단체, 국내 교육',
+      title: '제 2 대 컴퓨터 소프트웨어 공학과 학생회, 회장',
       url: '',
-      date: 'Mar 2019 ~ Nov 2018',
-    },
+      date: 'Mar 2013 ~ Feb 2014',
+    }
   ],
   award:[
-    {
-      title: '캡스톤 디자인 경진 대회 장려-RMD for Android',
-      url:'http://gmilbo.net/news/article.html?no=21412',
-      date:'Nov 2011',
-    },
     {
       title: '캡스톤 디자인 경진 대회 금상-Bubble Finder',
       url:'',
       date:'Nov 2012',
     },
-  ]
-
-
-};
-
-
-const test ={
-
-  career: [
     {
-       period:'2010 - 2012',
-       title:'java',
-       desc:'test'
+      title: '캡스톤 디자인 경진 대회 장려-RMD for Android',
+      url:'http://gmilbo.net/news/article.html?no=21412',
+      date:'Nov 2011',
     },
-    {
-       period:'2010 - 2012',
-       title:'java',
-       desc:'test'
-    }
-  ],
-  addExperience:[
-    'test1','test2'
   ]
-}
+};
